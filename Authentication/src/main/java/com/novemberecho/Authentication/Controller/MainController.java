489e88e34ecf7b08c67d5dc7bb048b65fa15a914
Pratyush Kumar <pratyushgta@gmail.com>
2024-03-25 23:45:58 +0530

    Update #4 25-03-2024

--- Authentication/src/main/java/com/novemberecho/Authentication/Controller/MainController.java
+++ Authentication/src/main/java/com/novemberecho/Authentication/Controller/MainController.java
@@ -5,14 +5,14 @@ import com.novemberecho.Authentication.DTO.UserRegistrationDto;
 import com.novemberecho.Authentication.Entity.User;
 import com.novemberecho.Authentication.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -32,19 +32,21 @@ public class MainController {
 
     @GetMapping("/home")
     public String home() {
-        return "redirect:http://localhost:9090";
+        return "redirect:http://localhost:9090/search/home";
         //return "homePage";
     }
 
+
     /*@GetMapping("/home")
     public String display() {
-        ResponseEntity<String> response = restTemplate.getForEntity("http://SearchModule:8081/search/X_home", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/homeX", String.class);
         if (response.getStatusCode().is2xxSuccessful()) {
-            return "redirect:/X_home";
+            return "redirect:/homeX";
         } else {
             return "accessDenied";
         }
     }*/
+
 
     @GetMapping("/view-accounts")
     public String admin_accounts() {
--- SearchModule/src/main/java/com/novemberecho/SearchModule/Controller/MainController.java
+++ SearchModule/src/main/java/com/novemberecho/SearchModule/Controller/MainController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/search")
 public class MainController {
-    @GetMapping
+    @GetMapping("/home")
     public String home() {
         return "X_homePage";
     }
