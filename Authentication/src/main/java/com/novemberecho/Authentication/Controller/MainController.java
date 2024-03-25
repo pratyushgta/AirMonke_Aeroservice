d25ad2e24e612c1a58060745bf5e7c9f65fe293a
Pratyush Kumar <pratyushgta@gmail.com>
2024-03-26 00:35:34 +0530

    Update #6 25-03-2024

--- Authentication/src/main/java/com/novemberecho/Authentication/Controller/MainController.java
+++ Authentication/src/main/java/com/novemberecho/Authentication/Controller/MainController.java
@@ -1,17 +1,11 @@
 package com.novemberecho.Authentication.Controller;
 
 
-import com.novemberecho.Authentication.DTO.UserRegistrationDto;
-import com.novemberecho.Authentication.Entity.User;
 import com.novemberecho.Authentication.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,21 +26,15 @@ public class MainController {
 
     @GetMapping("/home")
     public String home() {
-        return "redirect:http://localhost:9090/search/home";
+        //return "redirect:http://localhost:9090/search/home";
         //return "homePage";
-    }
-
-
-    /*@GetMapping("/home")
-    public String display() {
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/homeX", String.class);
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return "redirect:/homeX";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            return "redirect:http://localhost:9090/admin/admin-home";
         } else {
-            return "accessDenied";
+            return "redirect:http://localhost:9090/search/home";
         }
-    }*/
-
+    }
 
     @GetMapping("/view-accounts")
     public String admin_accounts() {
@@ -57,6 +45,8 @@ public class MainController {
     public String accessDenied() {
         return "accessDenied";
     }
+
+}
 
     /*@GetMapping
     public String currentUser(@ModelAttribute("user") UserRegistrationDto userDto, BindingResult result, Model model) {
@@ -70,4 +60,14 @@ public class MainController {
 
         return "homePage"; //this is the name of my template
     }*/
-}
+
+
+ /*@GetMapping("/home")
+    public String display() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/homeX", String.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return "redirect:/homeX";
+        } else {
+            return "accessDenied";
+        }
+    }*/
